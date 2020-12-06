37efe028f2334e733cc0f01b0974d46a8674ee11
Ubuntu <ubuntu@ip-172-31-31-126.us-east-2.compute.internal>
2020-12-06 05:11:17 +0000

    create stylists' signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  get 'home/index'
+  get 'home/index' => 'home#index'
   get 'registration/stylists' => 'stylists#new'
 
   devise_for :users,:controllers => {

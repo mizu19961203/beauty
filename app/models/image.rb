467a1e5624e6d819c4ed0e9f37aaf3be37c506b3
mizu19961203 <mizu19961203@gmail.com>
2021-01-31 05:51:52 +0000

    heroku-S3-setup-1.1

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,4 @@
 class Image < ApplicationRecord
 	belongs_to :shop, optional: true
   	mount_uploader :image_url, ImageUploader
-
-  	if Rails.env.development?
-	  storage :file
-	elsif Rails.env.test?
-	  storage :file
-	else
-	  storage :fog
-	end
 end
